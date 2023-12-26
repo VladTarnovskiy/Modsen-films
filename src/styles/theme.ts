5179df02e7f08765320f4e0413be78dfbb846626
vladtarnovskiy <vladtarnovski989@gmail.com>
2023-12-26 11:48:59 +0300

    feat: add styled default theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export const baseTheme = {
+  colors: {
+    primary: '#7986cb',
+    secondary: '#2b2b2b',
+    success: '#4caf50',
+    danger: '#f44336 ',
+
+    bg: '#E5E4E8',
+    font: '#19191B',
+  },
+
+  media: {
+    xl: '(max-width: 1140px)',
+    l: '(max-width: 960px)',
+    m: '(max-width: 720px)',
+    s: '(max-width: 540px)',
+    xs: '(max-width: 540px)',
+  },
+};
