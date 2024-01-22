a8adc27b31dd16cfa492ab15b1a4177d4c049ca5
vladtarnovskiy <vladtarnovski989@gmail.com>
2024-01-22 10:03:01 +0300

    refactor: replace router path to enum

--- src/router.tsx
+++ src/router.tsx
@@ -4,24 +4,30 @@ import { Layout } from './pages/Layout';
 import { MainPage } from './pages/Main';
 import { NotFoundPage } from './pages/NotFound';
 
+enum Router {
+  mainPage = '/',
+  detailsPage = 'details/:detailsId',
+  notFoundPage = '*',
+}
+
 export const routes = [
   {
     element: <Layout />,
     errorElement: <RouterErrorBoundary />,
-    path: '/',
+    path: Router.mainPage,
     children: [
       {
         index: true,
         element: <MainPage />,
       },
       {
-        path: 'details/:detailsId',
+        path: Router.detailsPage,
         element: <DetailsPage />,
       },
     ],
   },
   {
-    path: '*',
+    path: Router.notFoundPage,
     element: <NotFoundPage />,
   },
 ];
